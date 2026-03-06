48a9ac4c2ff1cc64b5710b77fe5224da5f64102d
iislam-uncc <iislam@charlotte.edu>
2026-03-06 11:14:02 -0500

    Merged checkDistance into formatConeOutput and merged formatConeOutput into Cone.

--- Classes/Cone.m
+++ Classes/Cone.m
@@ -311,5 +311,49 @@ classdef Cone < Feature
             ang = mod(qq(7), 2*pi);
             r   = f(qq).*cos(ang) + g(qq).*sin(ang) - qq(8);
         end
+
+        function [pnt, dir, ang, dist, apex] = formatConeOutput(ansVec, cent)
+
+            rawPnt = [ansVec(1), ansVec(2), ansVec(3)] + cent;
+            dir = -[ansVec(4), ansVec(5), ansVec(6)];
+            ang = ansVec(7);
+            rawDist = ansVec(8);
+        
+            dir = dir / norm(dir);   % convert to unit vector
+        
+            % Ensure angle is positive
+            if ang < 0
+                ang = -ang;
+                dir = -dir;
+            end
+        
+            % Normalization of angle (Shakarji, 1998 convention)
+            if ang > pi
+                ang = mod(ang, pi);
+                dir = -dir;
+            end
+            if ang > pi/2
+                ang = pi - ang;
+            end
+        
+            % Compute axis point closest to centroid
+            pnt = projPnts2Line(rawPnt, cent, dir);
+        
+            % Compute orthogonal distance to surface at pnt and apex location
+            m = rawDist * cos(ang);
+            n = rawDist * sin(ang);
+        
+            v = n + (m / tan(ang));
+            apex = rawPnt - v * dir;
+        
+            pnt2a = norm(pnt - apex);
+            dist = pnt2a * sin(ang);
+        
+            % Ensure direction faces toward cone opening
+            n = dot(apex - pnt, dir);
+            if n > 0
+                dir = -dir;
+            end
+        end
     end
 end
--- UtilityFunc/formatConeOutput.m
+++ UtilityFunc/formatConeOutput.m
@@ -1,18 +1,18 @@
 function [pnt, dir, ang, dist, apex] = formatConeOutput(ansVec, cent)
-    
+
     rawPnt = [ansVec(1), ansVec(2), ansVec(3)] + cent;
     dir = -[ansVec(4), ansVec(5), ansVec(6)];
     ang = ansVec(7);
     rawDist = ansVec(8);
 
     dir = dir / norm(dir);   % convert to unit vector
-    
+
     % Ensure angle is positive
     if ang < 0
         ang = -ang;
         dir = -dir;
     end
-    
+
     % Normalization of angle (Shakarji, 1998 convention)
     if ang > pi
         ang = mod(ang, pi);
@@ -21,15 +21,22 @@ function [pnt, dir, ang, dist, apex] = formatConeOutput(ansVec, cent)
     if ang > pi/2
         ang = pi - ang;
     end
-    
+
     % Compute axis point closest to centroid
     pnt = projPnts2Line(rawPnt, cent, dir);
 
-    % Compute orthogonal distance to surface at pnt
-    [dist, apex] = checkDistance(rawPnt, pnt, dir, rawDist, ang);
+    % Compute orthogonal distance to surface at pnt and apex location
+    m = rawDist * cos(ang);
+    n = rawDist * sin(ang);
+
+    v = n + (m / tan(ang));
+    apex = rawPnt - v * dir;
+
+    pnt2a = norm(pnt - apex);
+    dist = pnt2a * sin(ang);
 
     % Ensure direction faces toward cone opening
-    n = (apex - pnt) / dir;
+    n = dot(apex - pnt, dir);
     if n > 0
         dir = -dir;
     end
